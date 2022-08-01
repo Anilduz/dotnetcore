8fdfc3082824b001bf9c58dbe553be3028da358b
Anilduz <anilduz34@gmail.com>
2022-08-02 02:29:47 +0300

    web-apies

---
+++ WEBAPI1/Hello/Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hello.Controllers;
+
+[ApiController]
+[Route("api/[controller]s")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+
+        //api/WheatherForecast?id=3
+     [HttpGet]
+    public IEnumerable<WeatherForecast> GetById([FromQuery] string id )
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+
+        //api/WheatherForecast/3
+      [HttpGet("{id}")]
+    public IEnumerable<WeatherForecast> GetForecasts( string id )
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
